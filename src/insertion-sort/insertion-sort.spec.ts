af9903ff26b89dd9de951925899b3ba913795577
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2022-09-17 22:54:47 +0300

    insert sort: update type definition and test cases

--- src/insertion-sort/insertion-sort.spec.ts
+++ src/insertion-sort/insertion-sort.spec.ts
@@ -2,8 +2,10 @@ import { insertionSort } from "./insertion-sort";
 
 describe("insertion sort", () => {
   it.each`
-    value        | result
-    ${[1, 3, 2]} | ${[1, 2, 3]}
+    value                              | result
+    ${[-3, -1, 0, 1, 3]}               | ${[-3, -1, 0, 1, 3]}
+    ${[7, -5, 3, -7, 0, -1, 5, 1, -3]} | ${[-7, -5, -3, -1, 0, 1, 3, 5, 7]}
+    ${[3, 1, 0, -1, -3]}               | ${[-3, -1, 0, 1, 3]}
   `("test: turn $value into $result", ({ value, result }) => {
     expect(insertionSort(value)).toEqual(result);
   });
--- src/insertion-sort/insertion-sort.ts
+++ src/insertion-sort/insertion-sort.ts
@@ -1,9 +1,20 @@
-export const insertionSort = (arr: number[]) => {
-  let i, j, item;
+export type ComparatorFn<T = number> = (prev: T, curr: T) => boolean;
+
+export type SortFn<T = number> = (arr: T[], comparator: ComparatorFn<T>) => T[];
+
+const defaultComparator = <T = number>(prev: T, curr: T) => {
+  return prev < curr;
+};
+
+export const insertionSort = <T = number>(
+  arr: T[],
+  comparator: ComparatorFn<T> = defaultComparator
+) => {
+  let i: number, j: number, item: T;
   for (i = 1; i < arr.length; i++) {
-    item = arr[i] as number;
-    for (j = i; j > 0 && item < (arr[j - 1] as number); j--) {
-      arr[j] = arr[j - 1] as number;
+    item = arr[i] as T;
+    for (j = i; j > 0 && comparator(item, arr[j - 1] as T); j--) {
+      arr[j] = arr[j - 1] as T;
     }
     arr[j] = item;
   }
