cd590dfc2ecf1fdf0c8f9b3ce5cec24d288202ca
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2022-09-17 23:21:03 +0300

    insert sort: add objects and comporators to test cases

--- src/sorts/insertion-sort/insertion-sort.spec.ts
+++ src/sorts/insertion-sort/insertion-sort.spec.ts
@@ -1,12 +1,31 @@
 import { insertionSort } from "./insertion-sort";
 
+const comparatorSpy = jest
+  .fn()
+  .mockImplementation((prev: number, curr: number) => prev < curr);
+
+const comparatorObjectSpy = jest
+  .fn()
+  .mockImplementation(
+    (prev: { v: number }, curr: { v: number }) => prev.v < curr.v
+  );
+
 describe("insertion sort", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it.each`
-    value                              | result
-    ${[-3, -1, 0, 1, 3]}               | ${[-3, -1, 0, 1, 3]}
-    ${[7, -5, 3, -7, 0, -1, 5, 1, -3]} | ${[-7, -5, -3, -1, 0, 1, 3, 5, 7]}
-    ${[3, 1, 0, -1, -3]}               | ${[-3, -1, 0, 1, 3]}
-  `("test: turn $value into $result", ({ value, result }) => {
-    expect(insertionSort(value)).toEqual(result);
+    value                              | result                             | comparator
+    ${[-3, -1, 0, 1, 3]}               | ${[-3, -1, 0, 1, 3]}               | ${undefined}
+    ${[7, -5, 3, -7, 0, -1, 5, 1, -3]} | ${[-7, -5, -3, -1, 0, 1, 3, 5, 7]} | ${undefined}
+    ${[3, 1, 0, -1, -3]}               | ${[-3, -1, 0, 1, 3]}               | ${comparatorSpy}
+    ${[{ v: -5 }, { v: 0 }, { v: 5 }]} | ${[{ v: -5 }, { v: 0 }, { v: 5 }]} | ${comparatorObjectSpy}
+    ${[{ v: 5 }, { v: -5 }, { v: 0 }]} | ${[{ v: -5 }, { v: 0 }, { v: 5 }]} | ${comparatorObjectSpy}
+  `("test: turn $value into $result", ({ value, result, comparator }) => {
+    expect(insertionSort(value, comparator)).toEqual(result);
+    if (comparator) {
+      expect(comparator).toBeCalled();
+    }
   });
 });
