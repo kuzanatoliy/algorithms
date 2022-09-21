2d6317d863865c465808d174d4cb4ef98da48d90
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2022-09-21 15:16:03 +0300

    chore: update unit tests

--- src/sorts/insertion-sort/insertion-sort.spec.ts
+++ src/sorts/insertion-sort/insertion-sort.spec.ts
@@ -1,35 +1,15 @@
 import { insertionSort } from "./insertion-sort";
-
-const comparatorSpy = jest
-  .fn()
-  .mockImplementation((prev: number, curr: number) => prev < curr);
-
-const comparatorObjectSpy = jest
-  .fn()
-  .mockImplementation(
-    (prev: { v: number }, curr: { v: number }) => prev.v < curr.v
-  );
+import { testCases } from "../test-utils";
 
 describe("insertion sort", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it.each`
-    value                              | result                             | comparator             | order
-    ${[-3, -1, 0, 1, 3]}               | ${[-3, -1, 0, 1, 3]}               | ${undefined}           | ${undefined}
-    ${[7, -5, 3, -7, 0, -1, 5, 1, -3]} | ${[-7, -5, -3, -1, 0, 1, 3, 5, 7]} | ${undefined}           | ${true}
-    ${[3, 1, 0, -1, -3]}               | ${[-3, -1, 0, 1, 3]}               | ${comparatorSpy}       | ${true}
-    ${[-3, -1, 0, 1, 3]}               | ${[3, 1, 0, -1, -3]}               | ${undefined}           | ${false}
-    ${[7, -5, 3, -7, 0, -1, 5, 1, -3]} | ${[7, 5, 3, 1, 0, -1, -3, -5, -7]} | ${undefined}           | ${false}
-    ${[3, 1, 0, -1, -3]}               | ${[3, 1, 0, -1, -3]}               | ${comparatorSpy}       | ${false}
-    ${[{ v: -5 }, { v: 0 }, { v: 5 }]} | ${[{ v: -5 }, { v: 0 }, { v: 5 }]} | ${comparatorObjectSpy} | ${undefined}
-    ${[{ v: 5 }, { v: -5 }, { v: 0 }]} | ${[{ v: -5 }, { v: 0 }, { v: 5 }]} | ${comparatorObjectSpy} | ${true}
-    ${[{ v: 5 }, { v: -5 }, { v: 0 }]} | ${[{ v: 5 }, { v: 0 }, { v: -5 }]} | ${comparatorObjectSpy} | ${false}
-  `(
-    "test: turn $value into $result",
-    ({ value, result, comparator, order }) => {
-      expect(insertionSort(value, comparator, order)).toEqual(result);
+  it.each(Object.entries(testCases))(
+    "test %s:",
+    (_, { value, result, comparator, order }) => {
+      expect(insertionSort(value as any, comparator, order)).toEqual(result);
       if (comparator) {
         expect(comparator).toBeCalled();
       }
---
+++ src/sorts/test-utils/cases.ts
@@ -0,0 +1,58 @@
+import { comparatorSpy, comparatorObjectSpy } from "./spies";
+
+export const testCases = [
+  {
+    value: [-3, -1, 0, 1, 3],
+    result: [-3, -1, 0, 1, 3],
+    comparator: undefined,
+    order: undefined,
+  },
+  {
+    value: [7, -5, 3, -7, 0, -1, 5, 1, -3],
+    result: [-7, -5, -3, -1, 0, 1, 3, 5, 7],
+    comparator: undefined,
+    order: true,
+  },
+  {
+    value: [3, 1, 0, -1, -3],
+    result: [-3, -1, 0, 1, 3],
+    comparator: comparatorSpy,
+    order: true,
+  },
+  {
+    value: [-3, -1, 0, 1, 3],
+    result: [3, 1, 0, -1, -3],
+    comparator: undefined,
+    order: false,
+  },
+  {
+    value: [7, -5, 3, -7, 0, -1, 5, 1, -3],
+    result: [7, 5, 3, 1, 0, -1, -3, -5, -7],
+    comparator: undefined,
+    order: false,
+  },
+  {
+    value: [3, 1, 0, -1, -3],
+    result: [3, 1, 0, -1, -3],
+    comparator: comparatorSpy,
+    order: false,
+  },
+  {
+    value: [{ v: -5 }, { v: 0 }, { v: 5 }],
+    result: [{ v: -5 }, { v: 0 }, { v: 5 }],
+    comparator: comparatorObjectSpy,
+    order: undefined,
+  },
+  {
+    value: [{ v: -5 }, { v: 0 }, { v: 5 }],
+    result: [{ v: 5 }, { v: 0 }, { v: -5 }],
+    comparator: comparatorObjectSpy,
+    order: false,
+  },
+  {
+    value: [{ v: 5 }, { v: -5 }, { v: 0 }],
+    result: [{ v: -5 }, { v: 0 }, { v: 5 }],
+    comparator: comparatorObjectSpy,
+    order: true,
+  },
+];
