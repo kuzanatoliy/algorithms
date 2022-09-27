564199efeb58c01b4549bef34c637e16cf73e436
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2022-09-27 15:39:31 +0300

    feat: bubble sort

---
+++ src/sorts/bubble-sort/bubble-sort.spec.ts
@@ -0,0 +1,15 @@
+import { bubbleSort } from './bubble-sort';
+import { testCases } from '../test-utils';
+
+describe('bubble sort', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it.each(Object.entries(testCases))('test %s:', (_, { value, result, comparator, order }) => {
+    expect(bubbleSort(value as any, comparator, order)).toEqual(result);
+    if (comparator) {
+      expect(comparator).toBeCalled();
+    }
+  });
+});
